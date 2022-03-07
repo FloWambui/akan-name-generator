a8b336b0096055383a03e02eae41603f0dcffc10
FloWambui <gflorencewambui@gmail.com>
2022-03-07 03:23:56 +0300

    declare the bithday selection functions

--- script/script.js
+++ script/script.js
@@ -12,4 +12,17 @@ function verify(){
   var days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var femaleNames=["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"];
   var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+
+  if(day <= 0 || day >31){
+    document.getElementById("error").innerHTML = "Enter Valid Date."
+  }
+
+  else if(month <=0 || month >12){
+    document.getElementById("error").innerHTML = "Enter a Valid Month."
+  }
+
+  else if ((female === false) && (male === false)){
+    document.getElementById("error").innerHTML = "Select Gender"
+  }
 }
